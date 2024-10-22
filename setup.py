7866118deee355fe061b2cca6c83d61ae40db814
Miguel Flores-Acton <mfacton1@gmail.com>
2024-10-21 21:41:49 -0400

    Finish package

--- setup.py
+++ setup.py
@@ -2,24 +2,30 @@ from setuptools import setup, find_packages
 
 setup(
     name='git_sync_tool',
-    version='0.1.0',
+    version='0.1.4',
     description='Easily sync repos across devices',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
+    license="The Unlicense",
     author='Miguel Flores-Acton',
     author_email='mfacton1@gmail.com',
     url='https://github.com/mfacton/git-sync-tool',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'colorama',
         'prettytable',
         'tqdm',
         'gitpython',
     ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+    ],
     python_requires='>=3.6',
     entry_points={
         'console_scripts': [
-            'gsync=gsync:main',
+            'gsync=gsync.gsync:main',
         ],
     },
 )
